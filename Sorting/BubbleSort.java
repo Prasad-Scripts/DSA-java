7f8f9f4d7e0bd74fa4a2512cb0699bade929830e
Hari Prasad <itsprasadsclick2906@gmail.com>
2026-08-03 21:59:50 +0530

    Finalized Code

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,46 +1,46 @@
 import java.util.Scanner;
 
 class BubbleSort {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter Size of the array: ");
-		int Size = scanner.nextInt();
-		int[] array = new int[Size];
-		
-		System.out.print("Enter " + Size + " Elements: ");
-		for(int i=0;i<Size;i++) {
-			array[i] = scanner.nextInt();
-		}
-		
-		System.out.print("Before Sorting: ");
-		for(int nums : array) {
-			System.out.print(nums + " ");
-		}
-		
-		int []arrays = BubbleSortMethod(array,Size);
-		
-		System.out.println();
-		System.out.print("After Sorting: ");
-		for(int nums : array) {
-			System.out.print(nums + " ");
-		}		
-	}
-	
-	static int[] BubbleSortMethod(int[] array, int Size) {
-		int i,j,temp;
-		
-		for(i=0;i<Size;i++) {
-			for(j=0;j<Size - 1;j++) {
-				if(array[j] > array[j+1]) {
-					temp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = temp;
-				}
-			}
-		}
-		
-		return array;
-		
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.print("Enter " + size + " Elements: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.print("Before Sorting: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+
+        BubbleSortMethod(array, size);
+
+        System.out.println();
+        System.out.print("After Sorting: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+
+    static void BubbleSortMethod(int[] array, int size) {
+        int temp;
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
