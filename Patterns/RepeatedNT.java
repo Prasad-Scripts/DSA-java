2a9736be8d11a3b964bf8f0c4577027f53ecc16e
Prasad-Scripts <itsprasadsclick2906@gmail.com>
2026-08-14 14:17:45 +0530

    Repeated Number RightAngle Pattern

---
+++ Patterns/RepeatedNT.java
@@ -0,0 +1,20 @@
+// Repeated Number Triangle
+
+import java.util.Scanner;
+
+class RepeatedNT {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+		
+		scanner.close();
+	}
+}
