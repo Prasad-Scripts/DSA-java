8674f6c13ac6486061bfbf7332f32a22f0695da1
Prasad-Scripts <itsprasadsclick2906@gmail.com>
2026-08-16 12:28:19 +0530

    InvertedNumberTriangle Pattern

---
+++ Patterns/InvertedNumberTriangle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class InvertedNumberTriangle {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		for(int i=n;i>0;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		
+		scanner.close();
+	}
+}
+
+/*
+
+OUTPUT - n : 5 
+
+	1 2 3 4 5
+	1 2 3 4
+	1 2 3
+	1 2 
+	1
+		
+*/
